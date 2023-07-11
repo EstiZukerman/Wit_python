b179105b1f5e7f03b2f06b334aa47cf5f70fd350
estiZukerman <hand2b20@mbjcomp.org.il>
2023-07-11 15:03:48 +0300

    add base_pass as property in file_handler

--- file_handler.py
+++ file_handler.py
@@ -1,2 +1,41 @@
+import os
+
+
 class FileHandler:
-    pass
+    working_directory = None
+
+    def __init__(self):
+        self.base_path = None
+
+    @property
+    def base_path(self):
+        if not self.base_path:
+            raise Exception("Not a wit repository")
+
+    @base_path.setter
+    def base_path(self, base_path):
+        self.base_path = base_path
+    @staticmethod
+    def create_dir(path):
+        pass
+
+    @classmethod
+    def find_base_path(cls):
+        if not cls.base_path:
+            raise Exception("Not a wit repository")
+
+        # TODO: find first dir's path with .wit in it
+        found = False
+        # TODO: handle not wit repo
+        # raise Exception("Not a wit repository")
+
+    @classmethod
+    def validate_path(cls, path):
+        full_path = os.path.join(cls.working_directory, path)
+        if not os.path.exists(full_path):
+            pass
+            # TODO: handle file doesn't exist
+
+    @classmethod
+    def copy_item(cls, origin, target):
+        pass
