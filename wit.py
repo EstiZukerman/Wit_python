d222f857485d299bd4e42739cc06f0d3b9b1bd6c
estiZukerman <hand2b20@mbjcomp.org.il>
2023-07-12 20:43:33 +0300

    logger class

--- wit.py
+++ wit.py
@@ -1,12 +1,35 @@
 from file_handler import FileHandler
+import logging
 
 
+class Logger:
+    logging_format = "%(asctime)s LEVEL: %(levelname)s MSG: %(message)s"
+    logging_file_path = r'C:\עבודות\Esti\bootcamp\Wit_python\.wit\loggers\logger.txt'
+    logging.basicConfig(filename=logging_file_path, format=logging_format, level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    @staticmethod
+    def logging_to_file(*args):
+        def wrapper(func):
+            def inner_wrapper():
+                logging_format = "%(asctime)s LEVEL: %(levelname)s MSG: %(message)s"
+                logging_file_path = r'C:\עבודות\Esti\bootcamp\Wit_python\.wit\loggers\logger.txt'
+                logging.basicConfig(filename=logging_file_path, format=logging_format, level=logging.DEBUG)
+                logger = logging.getLogger(__name__)
+                logger.info(func)
+                if len(args) > 0:
+                    func(args)
+                else:
+                    func()
+            return inner_wrapper
+        return wrapper
 class Wit:
 
     @staticmethod
     def validate_is_wit_repo():
         return FileHandler.find_wit_path()
 
+    @Logger.logging_to_file()
     @staticmethod
     def init():
         if Wit.validate_is_wit_repo():
@@ -28,4 +51,4 @@ class Wit:
         FileHandler.copy_item(item_to_move, staging_area_path)
 
 
-Wit.add(["exe.txt"])
+Wit.init()
